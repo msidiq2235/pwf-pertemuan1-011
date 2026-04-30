787df4b3eacad283f2ab5b11fad467ca242eb40b
msidiq2235 <muhsidiq2005@gmail.com>
2026-04-30 14:24:00 +0700

    add role di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg border dark:border-gray-700">
+                <div class="p-6 text-gray-900 dark:text-gray-100 font-medium text-lg">
+                    Role: <span class="text-indigo-500 dark:text-indigo-400 font-bold">{{ ucfirst(auth()->user()->role) }}</span>
                 </div>
             </div>
         </div>
